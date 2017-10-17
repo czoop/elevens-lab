ff7d923296b282372d3b8681a5e96184bd701dd6
Connor <czoop@me.com>
2017-10-17 14:46:53 -0700

    Initial Commit.

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+	private String rank;
+	private String suit;
+	private int points;
+	
+	public Card(String rank, String suit, int points) {
+		this.rank = rank;
+		this.suit = suit;
+		this.points = points;
+	}
+	
+}
