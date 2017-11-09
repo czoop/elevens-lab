fa60ce3043c5e50163263fdc265ba1e33a45a8a6
Connor <czoop@me.com>
2017-11-09 14:49:43 -0800

    Added ability to play game.

--- src/Board.java
+++ src/Board.java
@@ -3,11 +3,12 @@ public class Board {
 	private ArrayList<Card> cards;
 	private Deck deck;
 	
-	public Board(Deck deck) {
-		this.deck = deck;
+	public Board(Deck newDeck) {
+		cards = new ArrayList<Card>();
+		deck = newDeck;
 		deck.shuffle();
 		for (int i = 0; i < 11; i++) {
-			cards.add(this.deck.deal());
+			cards.add(deck.deal());
 		}
 	}
 	
@@ -28,25 +29,70 @@ public class Board {
 	}
 	
 	public boolean canMakeMove(int card1, int card2) {
-		if (card1 <= 9 && card2 <= 9) {
-			if (cards.get(card1).getPoints() + cards.get(card2).getPoints() == 11) {
+		if (card1 <= 9 && card2 <= 9 && cards.get(card1).getPoints() + cards.get(card2).getPoints() == 11) {
 				return true;
-			}
-			else {
-				return false;
-			}
 		}
 		else {
 			return false;
 		}
 	}
 	
-	public void makeMove(int card1, int card2) {
+	public boolean makeMove(int card1, int card2) {
 		if (canMakeMove(card1, card2) && deck.size() >= 2) {
 			cards.remove(card1);
 			cards.remove(card2);
 			cards.add(card1, deck.deal());
 			cards.add(card2, deck.deal());
+			return true;
 		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean makeMoveFaceCards(int card1, int card2, int card3) {
+		if (isFaceCard(card1) && isFaceCard(card2) && isFaceCard(card3)) {
+			int points1 = cards.get(card1).getPoints();
+			int points2 = cards.get(card2).getPoints();
+			int points3 = cards.get(card3).getPoints();
+			
+			if ((points1 == 11 || points1 == 12 || points1 == 13) && 
+					(points2 == 11 || points2 == 12 || points2 == 13) &&
+					(points3 == 11 || points3 == 12 || points3 == 13)) {
+				cards.remove(card1);
+				cards.remove(card2);
+				cards.remove(card3);
+				cards.add(card1, deck.deal());
+				cards.add(card2, deck.deal());
+				cards.add(card3, deck.deal());
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+	
+	public boolean isFaceCard(int card1) {
+		int points = cards.get(card1).getPoints();
+		if (points == 11 || points == 12 || points == 13) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean movesLeft() {
+		int dex1 = 0;
+		int dex2 = 0;
+		while (dex1 < cards.size()) {
+			while (dex2 < cards.size()) {
+				if (canMakeMove(dex1, dex2)) {
+					return true;
+				}
+				dex2++;
+			}
+		}
+		return false;
 	}
 }
--- src/BoardRunner.java
+++ src/BoardRunner.java
@@ -4,7 +4,38 @@ public class BoardRunner {
 		Scanner input = new Scanner(System.in);
 		Deck deck = new Deck();
 		Board board = new Board(deck);
+		int card1 = 0;
+		int card2 = 0;
+		int card3 = 0;
+		System.out.println("Board Created!");
 		
-		System.out.println(board.getLayoutAsString());
+		while (board.movesLeft()) {
+			System.out.println("-------------------------------------------");
+			System.out.println(board.getLayoutAsString());
+			System.out.println("What cards would you like to choose?");
+			System.out.print("Card 1: ");
+			card1 = input.nextInt();
+			System.out.print("Card 2: ");
+			card2 = input.nextInt();
+			if (board.isFaceCard(card1)) {
+				System.out.print("Card 3: ");
+				card3 = input.nextInt();
+				System.out.println(board.makeMoveFaceCards(card1, card2, card3));
+			}
+			else {
+			System.out.println("Made Move: " + board.makeMove(card1, card2));
+			}
+			
+		}
+		
+		System.out.println("\n" + 
+				" _____ ____  _      _____   ____  _     _____ ____ \n" + 
+				"/  __//  _ \\/ \\__/|/  __/  /  _ \\/ \\ |\\/  __//  __\\\n" + 
+				"| |  _| / \\|| |\\/|||  \\    | / \\|| | //|  \\  |  \\/|\n" + 
+				"| |_//| |-||| |  |||  /_   | \\_/|| \\// |  /_ |    /\n" + 
+				"\\____\\\\_/ \\|\\_/  \\|\\____\\  \\____/\\__/  \\____\\\\_/\\_\\\n" + 
+				"                                                   \n" + 
+				"");
+		
 	}
 }
